41943272270d480a90357cd05c73ce484bcf8f4a
bruno moreira <bruno.moreira@dryve.com.br>
2025-07-02 13:17:04 -0300

    fix: batch size

--- main.py
+++ main.py
@@ -42,6 +42,6 @@ from trainers.default import train
 root_dir = '/content/gdrive/MyDrive/datamining'
 # root_dir = './data'
 train(200, f'{root_dir}/dataset-balanced-roi',
-      'webdataset', 'maxvit_nano_rw_256', batch_size=64, hyperparams={'lr': 1e-3, 'gamma': 0.95, 'step_size': 10, 'weight_decay': 1e-2}, shuffle_size=23974)
+      'webdataset', 'maxvit_nano_rw_256', batch_size=128, hyperparams={'lr': 1e-3, 'gamma': 0.95, 'step_size': 10, 'weight_decay': 1e-2}, shuffle_size=23974)
 
 # 7. Visualização do treinamento com PCA
